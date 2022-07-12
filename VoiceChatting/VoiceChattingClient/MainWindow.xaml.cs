9effec8302b477947445691539fe7271d692a477
Xeno <xeno.song@outlook.com>
2022-07-12 22:22:17 +0900

    Add sample list view design code

--- VoiceChatting/VoiceChattingClient/MainWindow.xaml.cs
+++ VoiceChatting/VoiceChattingClient/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace VoiceChattingClient
 {
@@ -68,6 +56,10 @@ namespace VoiceChattingClient
         private void OnButtonMicrophoneClick(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Microphone Button Clicked");
+        }
+
+        private void buttonHeadset_Loaded(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
---
+++ VoiceChatting/VoiceChattingClient/UI/ScrollLayout.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace VoiceChattingClient.UI
+{
+    /// <summary>
+    /// Interaction logic for ScrollLayout.xaml
+    /// </summary>
+    public partial class ScrollLayout : UserControl
+    {
+        public class Name
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        private List<Name> items = new List<Name>();
+
+        public ScrollLayout()
+        {
+            InitializeComponent();
+
+            ListViewTest.ItemsSource = items;
+            AddItem("test1", "test1");
+            AddItem("test2", "test2");
+            AddItem("test3", "test3");
+            AddItem("test4", "test4");
+            AddItem("test5", "test5");
+            AddItem("test6", "test6");
+            AddItem("test7", "test7");
+            ListViewTest.Items.Refresh();
+        }
+
+        public void AddItem(string type, string value)
+        {
+            items.Add(new Name { FirstName = type, LastName = value });
+        }
+    }
+}
