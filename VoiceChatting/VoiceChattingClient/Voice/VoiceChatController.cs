e912f8c251ae9d5abf4e6e2b555d57bf3a05a9ae
Xeno <xeno.song@outlook.com>
2022-08-23 22:16:59 +0900

    음성 채팅 세션의 관리 규칙 및 P2P 연결 방식 등의 사전 작업 진행

---
+++ VoiceChatting/VoiceChattingClient/Voice/VoiceChatController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoiceChattingClient.Connection;
+using VoiceChattingClient.SoundSystem;
+
+namespace VoiceChattingClient.Voice
+{
+    // TODO. https://www.notion.so/c5fbdf12031f4640913c33bf7086a686 | private
+    internal class VoiceChatController
+    {
+        public VoiceClient voiceClient;
+        public MicrophoneController microphoneController;
+        public SpeakerController speakerController;
+
+
+    }
+}
